b2916ecde372e65239eb69659b8d058de9f26ab7
angel cruz <angel.c@geopagos.com>
2020-10-17 19:35:02 -0300

    Fixed what needed fixing and squished some bugs. :bug:

--- src/LocationServiceProvider.php
+++ src/LocationServiceProvider.php
@@ -15,15 +15,15 @@ class LocationServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $migrationsFileName = [
-                'create_states_table.php',
-                'create_cities_table.php',
-                'create_neighborhoods_table.php',
+                '2020_10_17_000001_create_states_table.php',
+                '2020_10_17_000002_create_cities_table.php',
+                '2020_10_17_000003_create_neighborhoods_table.php',
             ];
 
             foreach ($migrationsFileName as $migration) {
                 if (! $this->migrationFileExists($migration)) {
                     $this->publishes([
-                        __DIR__ . "/../database/migrations/{$migration}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migration),
+                        __DIR__ . "/../database/migrations/{$migration}.stub" => database_path('migrations/' . $migration),
                     ], 'migrations');
                 }
             }
